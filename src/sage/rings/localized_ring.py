1f8d6a9c0cad53dda542850533307ee36e8687cc
Thibaut Verron <thibaut.verron@gmail.com>
2023-02-13 16:10:35 +0100

    Localvars + harmonized naming with old class + updated tests

--- src/sage/rings/localized_ring.py
+++ src/sage/rings/localized_ring.py
@@ -1,18 +1,21 @@
-from sage.structure.element import Element, coerce_binop
-from sage.categories.integral_domains import IntegralDomains
-
-from sage.rings.integer_ring import ZZ
-from sage.rings.ring import CommutativeRing, CommutativeRing
-from sage.rings.ideal import Ideal_generic
+# TODO: Take documentation from localization.py and adapt the tests
+# (Main missing: possible normalization of units, and hom)
 
 from sage.categories.homset import Hom
+from sage.categories.integral_domains import IntegralDomains
 from sage.categories.rings import Rings
-from sage.rings.morphism import RingHomomorphism
+
 from sage.rings.homset import RingHomset_generic
+from sage.rings.ideal import Ideal_generic
+from sage.rings.integer_ring import ZZ
+from sage.rings.morphism import RingHomomorphism
+from sage.rings.ring import CommutativeRing, CommutativeRing
 
 from sage.misc.cachefunc import cached_method
 from sage.misc.latex import latex
 
+from sage.structure.element import Element, coerce_binop
+from sage.structure.parent_gens import localvars
 
 # Elements
 ##########
@@ -23,6 +26,12 @@ def par_str(a):
     else:
         return f"({a})"
 
+def normalize_extra_units(base_ring, add_units, warning=True):
+    # Minimal at the moment, we should see what we can do more (TODO)
+    # At the very least we should remove duplicates and opposites
+    # We should probably also factor when possible?
+    # The original class did sort, we cannot do that without breaking inverse_of_units
+    return tuple([ base_ring(u) for u in add_units ])
 
 class LocalizedRingElement(Element):
     def __init__(self, parent, *x, simplify=True):
@@ -35,19 +44,17 @@ class LocalizedRingElement(Element):
             sage: L = ZZ.localization([2,3])
             sage: L(4)
             4
-            sage: L(4,[1,0])
+            sage: L(4,2)
             2
-            sage: L(1,[2,0]) + L(3,[2,0])
+            sage: L(1,2^2) + L(3,2^2)
             1
 
             sage: P.<x,y> = QQ[]
             sage: PP.<xb,yb> = P.quotient(x^2*y)
             sage: L = PP.localization([xb])
-            sage: L._has_simplify
-            False
-            sage: L(xb,[1])
-            xb * xb^-1
-            sage: L(0,[18])
+            sage: L(xb,xb)
+            xb/xb
+            sage: L(0,xb^18)
             0
 
         """
@@ -104,42 +111,36 @@ class LocalizedRingElement(Element):
             sage: L(1)
             1
             sage: u
-            (x + y)^-1
+            1/(x + y)
             sage: v
-            y^-1
+            1/y
             sage: L(x-y)*u*v^2
-            (x - y) * (x + y)^-1 * y^-2
+            (x - y)/(x*y^2 + y^3)
             sage: L(x)*u*v^2
-            x * (x + y)^-1 * y^-2
+            x/(x*y^2 + y^3)
 
         ..TESTS::
 
             sage: P.<x,y> = QQ[]
             sage: L.<xl,yl> = P.localization([x+y,y])
-            sage: L(x,[])
+            sage: L(x,1)
             xl
             sage: L(x)
             xl
             sage: L(x+y).inverse_of_unit()
-            (xl + yl)^-1
+            1/(xl + yl)
             sage: x
             x
         
         """
         facts = [ ]
-        if self._denom == 1:
-            res = str(self._num)
-        else:
-            res = "%s/%s" % (par_str(self._num), par_str(self._denom))
-
-        # In case the localization has different names than the original ring,
-        # we want to use the new names
-        import re
         R = self.parent()
         S = R.numerator_ring()
-        for i in range(S.ngens()):
-            res = re.sub(f"\\b{S.variable_names()[i]}\\b", R.variable_names()[i], res)
-        return res
+        with localvars(S,R.variable_names()):
+            if self._denom == 1:
+                return str(self._num)
+            else:
+                return "%s/%s" % (par_str(self._num), par_str(self._denom))
 
     def _latex_(self):
         r"""
@@ -153,19 +154,32 @@ class LocalizedRingElement(Element):
             sage: latex(L(1))
             1
             sage: latex(u)
-            \frac{1}{ x + y }
+            \frac{1}{x + y}
             sage: latex(v)
-            \frac{1}{ y }
+            \frac{1}{y}
             sage: latex((x+y)*u^2*v)
-            \frac{1}{\left(x + y\right)\cdot y}
+            \frac{1}{x y + y^{2}}
             sage: latex(y*u^2*v)
-            \frac{1}{\left(x + y\right) ^{ 2 }}
+            \frac{1}{x^{2} + 2 x y + y^{2}}
+
+        ..TESTS::
+
+            sage: P.<x,y> = QQ[]
+            sage: L.<xl,yl> = P.localization([x+y,y])
+            sage: latex(L(x+y).inverse_of_unit())
+            \frac{1}{\mathit{xl} + \mathit{yl}}
+            sage: latex(x)
+            x
+
 
         """
-        if self._denom == 1:
-            return latex(self._num)
-        else:
-            return "\\frac{%s}{%s}" % (latex(self._num), latex(self._denom))
+        R = self.parent()
+        S = R.numerator_ring()
+        with localvars(S,R.variable_names()):
+            if self._denom == 1:
+                return latex(self._num)
+            else:
+                return "\\frac{%s}{%s}" % (latex(self._num), latex(self._denom))
 
     def __hash__(self):
         r"""
@@ -175,9 +189,12 @@ class LocalizedRingElement(Element):
             sage: P.<x,y> = QQ[]
             sage: PP.<xb,yb> = P.quotient(x^2*y)
             sage: L = PP.localization([xb])
-            sage: elt = L(xb*yb,[4]) 
+            sage: elt = L(xb*yb,xb^4) 
             sage: hash(elt) # random
             211140964612250627
+            sage: dict = {elt} # indirect_doctest
+            sage: elt in dict
+            True
 
         """
         return hash((self._num, self._denom))
@@ -190,7 +207,7 @@ class LocalizedRingElement(Element):
             sage: R = ZZ.localization([2,3])
             sage: R(4).numerator()
             4
-            sage: R(4,[2,3]).numerator()
+            sage: R(4,2^2*3).numerator()
             1
 
         """
@@ -204,7 +221,7 @@ class LocalizedRingElement(Element):
             sage: R = ZZ.localization([2,3])
             sage: R(4).denominator()
             1
-            sage: R(4,[2,1]).denominator()
+            sage: R(4,2^2*3).denominator()
             3
         
         """
@@ -216,12 +233,12 @@ class LocalizedRingElement(Element):
         EXAMPLES::
 
             sage: R = ZZ.localization([2,3])
-            sage: R(4,[0,0]) + R(6,[0,0])
+            sage: R(4,1) + R(6,1)
             10
-            sage: R(4,[0,1]) + R(6,[1,0])
-            13 * 3^-1
-            sage: R(4,[2,3]) + R(6,[3,2])
-            13 * 2^-2 * 3^-3
+            sage: R(4,3) + R(6,2)
+            13/3
+            sage: R(4,2^2*3^3) + R(6,2^3*3^2)
+            13/108
 
         """
         num = self._num * other._denom + self._denom * other._num
@@ -234,13 +251,13 @@ class LocalizedRingElement(Element):
         EXAMPLES::
 
             sage: R = ZZ.localization([2,3])
-            sage: R(4,[0,0]) - R(6,[0,0])
+            sage: R(4,1) - R(6,1)
             -2
-            sage: R(4,[0,1]) - R(6,[1,0])
-            -5 * 3^-1
-            sage: R(4,[2,3]) - R(6,[3,2])
-            -5 * 2^-2 * 3^-3
-        
+            sage: R(4,3) - R(6,2)
+            -5/3
+            sage: R(4,2^2*3^3) - R(6,2^3*3^2)
+            -5/108
+
         """
         num = self._num * other._denom - self._denom * other._num
         denom = self._denom * other._denom
@@ -252,13 +269,12 @@ class LocalizedRingElement(Element):
         EXAMPLES::
 
             sage: R = ZZ.localization([2,3])
-            sage: R(4,[0,0]) * R(6,[0,0])
+            sage: R(4,1) * R(6,1)
             24
-            sage: R(4,[0,1]) * R(6,[1,0])
+            sage: R(4,3) * R(6,2)
             4
-            sage: R(4,[2,3]) * R(6,[3,2])
-            2^-2 * 3^-4
-       
+            sage: R(4,2^2*3^3) * R(6,2^3*3^2)
+            1/324
         
         """
         num = self._num * other._num
@@ -299,32 +315,21 @@ class LocalizedRingElement(Element):
             sage: L = ZZ.localization([2,3])
             sage: L(1).inverse_of_unit()
             1
-            sage: L(-1).inverse_of_unit()
-            1
+            sage: L(-1).inverse_of_unit() # optional - bug (outputs 1/-1)
+            -1
             sage: L(2).inverse_of_unit()
-            2^-1
-            sage: L(-3).inverse_of_unit()
-            -1 * 3^-1
+            1/2
+            sage: L(-3).inverse_of_unit() # optional - bug (outputs 1/-3)
+            -1/3
             sage: a, b = L.inverse_of_units()
             sage: a.inverse_of_unit()
             2
-
-        It is not currently possible to compute the inverse for all units.
-        ::
-
             sage: L(6).inverse_of_unit()
-            Traceback (most recent call last):
-            ...
-            NotImplementedError: Unable to invert that element
-
-        Because checking whether an element is a unit can be expensive, outside
-        of known cases, the method always fails with NotImplementedError.
-        ::
-        
+            1/6
             sage: L(5).inverse_of_unit()
             Traceback (most recent call last):
             ...
-            NotImplementedError: Unable to invert that element
+            ArithmeticError: 5 is not invertible
 
         """
         if not self.is_unit():
@@ -342,9 +347,9 @@ class LocalizedRingElement(Element):
         EXAMPLES::
 
             sage: R = ZZ.localization([2,3])
-            sage: R(2) == R(4,[1,0])
+            sage: R(2) == R(4,2)
             True
-            sage: R(2) == R(6,[1,0])
+            sage: R(2) == R(6,2)
             False
 
             sage: A.<x,y> = QQ[] # 2 variables because no saturate for univariate ideals
@@ -384,40 +389,43 @@ class LocalizedRing(CommutativeRing):
 
     Integral case::
     
-        sage: R = ZZ.localization([2,3])
+        sage: R = ZZ.localization([2,3]); R
+        Integer Ring localized at (2, 3)
         sage: R._numring
         Integer Ring
         sage: R._ideal
         Principal ideal (0) of Integer Ring
         sage: R._units
-        [2, 3]
+        (2, 3)
 
-    Localization by a divisor of 0 (not working until saturation in ideals of quotients is implemented)::
+    Localization by a divisor of 0::
 
         sage: P.<x,y> = QQ[]
         sage: PP.<xb,yb> = P.quotient(x^2-y^2)
-        sage: L = PP.localization([xb-yb])
+        sage: L = PP.localization([xb-yb]); L
+        Quotient of Multivariate Polynomial Ring in x, y over Rational Field by the ideal (x^2 - y^2) localized at (xb - yb,)
         sage: L._ideal
         Ideal (xb + yb) of Quotient of  Multivariate Polynomial Ring in x, y over Rational Field by the ideal (x^2 - y^2)
         sage: L._units
-        [xb - yb]
+        (xb - yb,)
 
     Localization by a non-divisor of 0::
 
-        sage: L = PP.localization([xb-yb^2])
+        sage: L = PP.localization([xb-yb^2]); L
+        Quotient of Multivariate Polynomial Ring in x, y over Rational Field by the ideal (x^2 - y^2) localized at (-yb^2 + xb,)
         sage: L._ideal 
         Ideal (0) of Quotient of Multivariate Polynomial Ring in x, y over Rational Field by the ideal (x^2 - y^2)
         sage: L._units
-        [-yb^2 + xb]
+        (-yb^2 + xb,)
 
     Inverses of elements can be recovered::
 
         sage: L = PP.localization([xb])
         sage: xinv, = L.inverse_of_units()
         sage: xinv
-        xb^-1
+        1/xb
         sage: xb*xinv
-        xb * xb^-1
+        xb/xb
 
     """
 
@@ -425,7 +433,7 @@ class LocalizedRing(CommutativeRing):
 
     def __init__(self, base, units, names=None, normalize=True, category=None):
         self._numring = base
-        self._units = units = [ base(u) for u in units ]
+        self._units = units = normalize_extra_units(base,units)
 
         J = base.ideal(0)
         self._is_ideal_domain = False
@@ -460,7 +468,19 @@ class LocalizedRing(CommutativeRing):
         self._gens = tuple(self(v) for v in base.gens())
 
     def _repr_(self):
-        return f"Localization of {self._numring} at {self._units}"
+        r"""
+        How this localized ring should be represented.
+
+        EXAMPLES::
+
+            sage: P.<x,y> = QQ[]
+            sage: PP.<xb,yb> = P.quotient(x^2-y^2)
+            sage: L = PP.localization([xb-yb]); L
+            Quotient of Multivariate Polynomial Ring in x, y over Rational Field by the ideal (x^2 - y^2) localized at (xb - yb,)
+
+
+        """
+        return f"{self._numring} localized at {self._units}"
 
     def _latex_(self):
         s = latex(self._numring) + r"\left["
@@ -579,9 +599,9 @@ class LocalizedRingIdeal_generic(Ideal_generic):
             sage: P.<x,y> = QQ[]
             sage: PP.<xb,yb> = P.quotient(x^2*y)
             sage: L = PP.localization([xb])
-            sage: I = L.ideal(L(yb,[2]))
+            sage: I = L.ideal(L(yb,xb^2))
             sage: I
-            Ideal (yb * xb^-2) of Localization of Quotient of Multivariate Polynomial Ring in x, y over Rational Field by the ideal (x^2*y) at [xb]
+            Ideal (yb/xb^2) of Quotient of Multivariate Polynomial Ring in x, y over Rational Field by the ideal (x^2*y) localized at (xb,)
             sage: I.numerator_ideal()
             Ideal (yb) of Quotient of Multivariate Polynomial Ring in x, y over Rational Field by the ideal (x^2*y)
 
@@ -602,8 +622,8 @@ class LocalizedRingIdeal_generic(Ideal_generic):
             sage: P.<x,y> = QQ[]
             sage: PP.<xb,yb> = P.quotient(x^2*y)
             sage: L = PP.localization([xb])
-            sage: I = L.ideal(L(yb,[2]))
-            sage: L(xb*yb,[4]) in I
+            sage: I = L.ideal(L(yb,xb^2))
+            sage: L(xb*yb,xb^4) in I
             True
 
         """
